5745f69fc8429f8009a17e650e58960cbd1b5d0d
Gilliane Oswald <gilliane.c.oswald@gmail.com>
2015-03-15 22:18:07 -0600

    Project Creation
    - Initial push.
    - Created the Block class file.

---
+++ src/net/kymaeramystic/kmmsp/Block.java
@@ -0,0 +1,15 @@
+package net.kymaeramystic.kmmsp;
+
+/** 
+ * 
+ * @author Gilliane Oswald
+ * This class handles all of the block/entity related properties for the planner, such as block type 
+ * (full block, half-block, or entity) and texture.
+ *
+ */
+
+public class Block {
+	
+	//TODO: Write this class.
+
+}
